02befa7cb5a4d89fd3909addc2291ca884ea8300
dufugang <dufugang@yunlizhihui.com>
2018-12-07 13:45:56 +0800

    fix bug

---
+++ dozer-test-web/src/main/java/dozer/config/DozerMapperConfig.java
@@ -0,0 +1,24 @@
+package dozer.config;
+
+import com.github.dozermapper.spring.DozerBeanMapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+/**
+ * dozer映射配置
+ *
+ * @author dufugang
+ * @create 2018-10-22 下午2:46
+ */
+@Configuration
+public class DozerMapperConfig {
+    @Bean
+    public DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean(@Value("classpath*:dozer/*.xml") Resource[] resources)
+        throws Exception {
+        final DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean = new DozerBeanMapperFactoryBean();
+        dozerBeanMapperFactoryBean.setMappingFiles(resources);
+        return dozerBeanMapperFactoryBean;
+    }
+}
